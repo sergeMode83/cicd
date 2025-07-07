65cc9f4fc8390f528709169e0dcd24bb9e037e46
serge.baldacci <serge.baldacci@mode83.net>
2025-07-07 10:04:07 +0200

    test 22

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// eslint.config.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
